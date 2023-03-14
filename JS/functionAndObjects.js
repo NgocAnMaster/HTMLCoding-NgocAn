580c80a239178ab2a73172a4f36b7dce4fdc6a20
NgocAnMaster <lengocanthyh@gmail.com>
2023-03-14 21:52:33 +0700

    Modify files

---
+++ JS/functionAndObjects.js
@@ -0,0 +1,18 @@
+function add(){
+    let tex1 = "Nhap vao so thu nhat";
+    let num1 = prompt(tex1);
+    let num2 = prompt("Nhap vao s0 thu 2");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    return (num1 + num2);
+}
+
+function abc(num1){
+    num1 = num1 * num1;
+    return num1;
+}
+function changeArr(arr){
+    arr[0] = "def";
+}
+
+
---
+++ JS/jsFunctionFile.js
@@ -0,0 +1,15 @@
+function add(){
+    let tex1 = "Nhap vao so thu nhat"
+    let num1 = prompt(tex1);
+    let num2 = prompt("Nhap vao so thu 2");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    return num1+num2;
+}
+function abc(num1){
+    num1 = num1 * num1;
+    return num1;
+}
+function changeArr(arr){
+    arr[0] = "def";
+}
